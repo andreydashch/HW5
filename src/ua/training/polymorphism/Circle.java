d79d0935fa8a00b5519c3f3ada25caea69c49f7c
andreydashch <andreydashch@gmail.com>
2019-11-29 09:58:19 +0200

    Add class Circle

---
+++ src/ua/training/polymorphism/Circle.java
@@ -0,0 +1,25 @@
+/*
+ * @(#) Circle.java
+ *
+ * This software can be used by anyone
+ * with no limit. But developer do not
+ * granite its proper working.
+ */
+
+
+package ua.training.polymorphism;
+
+/**
+ * @author      Dashchyk Andrey
+ */
+public class Circle extends Point {
+    private double radius;
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
