d511c74d5d82543abb83b5bc8ced13a53d2a9f52
andreydashch <andreydashch@gmail.com>
2019-11-29 09:43:29 +0200

    Correct format of code

---
+++ src/ua/training/polymorphism/Main.java
@@ -0,0 +1,22 @@
+/*
+ * @(#) Main.java
+ *
+ * This software can be used by anyone
+ * with no limit. But developer do not
+ * granite its proper working.
+ */
+
+
+package ua.training.polymorphism;
+
+/**
+ * Polymorphism samples
+ *
+ * @author      Dashchyk Andrey
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
