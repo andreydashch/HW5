e86e43c1588fdea1f82170b2112463d87c44f9d4
andreydashch <andreydashch@gmail.com>
2019-11-29 09:43:45 +0200

    Add class Point

--- src/ua/training/polimorphism/Main.java
+++
@@ -1,8 +0,0 @@
-package ua.training.polimorphism;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/ua/training/polymorphism/Point.java
@@ -0,0 +1,16 @@
+/*
+ * @(#) Point.java
+ *
+ * This software can be used by anyone
+ * with no limit. But developer do not
+ * granite its proper working.
+ */
+
+
+package ua.training.polymorphism;
+
+/**
+ * @author      Dashchyk Andrey
+ */
+public class Point {
+}
