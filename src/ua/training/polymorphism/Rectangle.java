62ba8f435bbe0caadcdd05edf8b70a29e9daf98b
andreydashch <andreydashch@gmail.com>
2019-11-29 10:16:36 +0200

    Add constructors Rectangle

--- src/ua/training/polymorphism/Rectangle.java
+++ src/ua/training/polymorphism/Rectangle.java
@@ -4,6 +4,18 @@ public class Rectangle extends Point {
     private double x1;
     private double y1;
 
+    Rectangle(double x1, double y1) {
+        super();
+        this.x1 = x1;
+        this.y1 = y1;
+    }
+
+    Rectangle(double x, double y, double x1, double y1) {
+        super(x, y);
+        this.x1 = x1;
+        this.y1 = y1;
+    }
+
     public void setX1(double x1) {
         this.x1 = x1;
     }
